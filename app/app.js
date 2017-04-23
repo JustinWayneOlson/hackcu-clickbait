a15ff025a282f67e76e92df7bbac26c5605d5cd8
Patrick Zawadzki <zawadzkip@msoe.edu>
2017-04-22 23:52:11 -0600

    aws updates

--- app/app.js
+++ app/app.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: false
+}));
 var request = require('request');
 var API_KEYS = require('./API_KEYS');
 var summary = require('node-summary');
+var urlService = require('./URLService.js');
+var aws = require('aws-sdk');
+var machineLearning = new aws.MachineLearning({
+    'region': 'us-east-1',
+});
 
 app.post('/summarize', function (req, res) {
 
-     if (!req.params.url) {
+     if (req.body && !req.body.urlToCheck) {
           return res.status(400).send({
-             status: 'error',
+             complete: 'false',
              summary: '',
              url: '',
           });
@@ -16,7 +26,42 @@ app.post('/summarize', function (req, res) {
 
 });
 
+app.post('/checkarticle', function (req, res) {
+   if (req.body && !req.body.urlToCheck || !req.body.articleTitle) {
+        return res.status(400).send({
+           complete: 'false',
+           isClickBait: null,
+           percentLikely: null,
+        });
+   }
+   var cleanedUrl = urlService.cleanURL({
+       urlToClean:req.body.urlToCheck
+   });
+   var options = {
+           MLModelId: API_KEYS.ML_MODEL_ID,
+           Record: {
+               'Var1': req.body.articleTitle,
+               'Var2': cleanedUrl,
+           },
+           PredictEndpoint: API_KEYS.ML_ENDPOINT,
+   };
 
+   machineLearning.predict(options, function (err, data) {
+      if (err) {
+          return res.status(400).send({
+              complete: false,
+              isClickBait: null,
+              percentLikely: null,
+          });
+      }
+        return res.status(200).send({
+           complete: true,
+           isClickBait: data.Prediction.predictedValue,
+           percentLikely: data.Prediction.predictedScores[data.Prediction.predictedValue],
+        });
+   });
+
+});
 
 
 app.listen(3000, function () {
