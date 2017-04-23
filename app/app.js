c4d16ac2e12ec4d2c39885ec0bee8b3353d0195f
Patrick Zawadzki <zawadzkip@msoe.edu>
2017-04-22 20:43:03 -0600

    adding some summary stuff

--- app/app.js
+++ app/app.js
@@ -1,10 +1,24 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var request = require('request');
+var API_KEYS = require('./API_KEYS');
+var summary = require('node-summary');
 
-app.get('/', function (req, res) {
-     res.send('Hello World!')
-})
+app.post('/summarize', function (req, res) {
+
+     if (!req.params.url) {
+          return res.status(400).send({
+             status: 'error',
+             summary: '',
+             url: '',
+          });
+     }
+
+});
+
+
+
 
 app.listen(3000, function () {
      console.log('Example app listening on port 3000!')
-})
+});
